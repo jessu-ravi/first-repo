b168611d16365062c98dda284542bdf9eebf80d5
anto <anto@Josephs-MacBook-Pro.local>
2024-12-11 22:56:08 -0500

    Added src and pom file

---
+++ src/main/java/com/shy/firstrepo/FirstRepoApplication.java
@@ -0,0 +1,13 @@
+package com.shy.firstrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shy/firstrepo/FirstRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shy.firstrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
